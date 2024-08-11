0af4b89b41a0db194c0813fdb9280abe1298014a
argenestel <arpitpali68@gmail.com>
2024-08-11 18:13:14 +0530

    hello world :

---
+++ packages/contracts/out/ISeasonPassSystem.sol/ISeasonPassSystem.abi.json.d.ts
@@ -0,0 +1,15 @@
+declare const abi: [
+  {
+    "type": "function",
+    "name": "buySeasonPass",
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "payable"
+  }
+]; export default abi;
